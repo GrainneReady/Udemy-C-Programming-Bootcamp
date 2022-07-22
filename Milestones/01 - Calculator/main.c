98d2fa466552835c10b65fcfd35304a3b5aef15f
Grainne Ready <grainneready@gmail.com>
2022-07-22 21:53:19 +0100

    Edited Folder Names

---
+++ Milestones/01 - Calculator/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Addition: %d + %d = %d\n", 5, 2, 5+2);
+    printf("Subtraction: %d - %d = %d\n", 5, 2, 5-2);
+    printf("Multiplication: %d * %d = %d\n", 5, 2, 5*2);
+    printf("Division: %d / %d = %d\n", 5, 2, 5/2); 
+    printf("Remainder: %d %% %d = %d\n", 5, 2, 5%2);
+    return 0;
+}
